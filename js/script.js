d4704b1afc0166f79b50ef81ce51b9838db8f7dd
imsurajyd <imsurajyd@gmail.com>
2025-08-03 13:35:23 +0530

    All changed save

--- js/script.js
+++ js/script.js
@@ -33,15 +33,4 @@ function updateUI() {
 // Auto load if logged in
 updateUI();
 
-// SLIDER
-let slides = document.querySelectorAll(".slide");
-let currentIndex = 0;
-
-function showNextSlide() {
-    slides[currentIndex].classList.remove("active");
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add("active");
-}
-
-setInterval(showNextSlide, 6000);
 
